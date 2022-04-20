4d266335dc9f831af8f9720c4fbc6cf2d5db828b
D1MENSI0N <angel.uriot@gmail.com>
2022-04-20 02:55:40 +0200

    Fix cam bug spec

--- sources/client/js/player/spectator_mode.ts
+++ sources/client/js/player/spectator_mode.ts
@@ -1,30 +1,30 @@
 import { Camera } from '../renderer/camera.js';
 import { Player } from './player.js';
 
-export function events() {
-    const button_menu = document.querySelector('.spectator .svg_button') as SVGElement;
-    const button_game = document.querySelector('.spectator_div .svg_button') as SVGElement;
+export function events()
+{
+	const button_menu = document.querySelector('.spectator .svg_button') as SVGElement;
+	const button_game = document.querySelector('.spectator_div .svg_button') as SVGElement;
 
-    const spectator_div = document.querySelector('.spectator_div') as HTMLDivElement;
-    const connect = document.querySelector('.connect_div') as HTMLDivElement;
-    const leaderboard = document.querySelector('.leaderboard') as HTMLDivElement;
+	const spectator_div = document.querySelector('.spectator_div') as HTMLDivElement;
+	const connect = document.querySelector('.connect_div') as HTMLDivElement;
+	const leaderboard = document.querySelector('.leaderboard') as HTMLDivElement;
 
-    button_menu.addEventListener('click', e =>
-    {
-        e.preventDefault();
-        
-        Player.spectator = true;
-        Camera.init();
-        
-        spectator_div.style.visibility = 'visible';
-        leaderboard.style.visibility = 'visible';
+	button_menu.addEventListener('click', e =>
+	{
+		e.preventDefault();
 
-        connect.style.display = 'none';
-    });
+		Player.spectator = true;
 
-    button_game.addEventListener('click', e =>
-    {
-        e.preventDefault();
-        setTimeout(() => { location.reload(); }, 100);
-    });
+		spectator_div.style.visibility = 'visible';
+		leaderboard.style.visibility = 'visible';
+
+		connect.style.display = 'none';
+	});
+
+	button_game.addEventListener('click', e =>
+	{
+		e.preventDefault();
+		setTimeout(() => { location.reload(); }, 100);
+	});
 }
