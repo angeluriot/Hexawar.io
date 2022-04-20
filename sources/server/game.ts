7b11390b3e2f9093fd120293ab5026f231a9939e
arthurazambre <arthur@azambre.net>
2022-04-20 09:58:26 +0200

    sans le print

--- sources/server/game.ts
+++ sources/server/game.ts
@@ -252,7 +252,6 @@ export function troops_spawn()
 				nb_troops: cell.nb_troops
 			};
 
-			console.log((map_size * territory) ** (1/2));
 			// Add troops if there are not enough
 			if (change.nb_troops < Global.troops_spawn_max && spawn_chance > (map_size * territory) ** (1/2))
 			{
